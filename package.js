5205fbd06248e45eaeba712a32b63fa3d77ba189
Theodor Diaconu <diaconu.theodor@gmail.com>
2017-11-28 12:28:55 +0200

    bump to 0.1.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'cultofcoders:grapher-react',
-    version: '0.1.0',
+    version: '0.1.1',
     // Brief, one-line summary of the package.
     summary: 'Provides HOCs for easily wrapping components with Grapher queries',
     // URL to the Git repository containing the source code for this package.
