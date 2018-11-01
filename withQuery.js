a23e475a236bae746aba69a55fc0059fbe29f20e
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-11-01 13:43:26 +0100

    Add support for loadOnRefetch option

--- withQuery.js
+++ withQuery.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import defaults from './defaults';
-import {withTracker} from 'meteor/react-meteor-data';
+import { withTracker } from 'meteor/react-meteor-data';
 import withReactiveQuery from './lib/withReactiveQuery';
 import withQueryContainer from './lib/withQueryContainer';
 import withStaticQuery from './lib/withStaticQuery';
 import checkOptions from './lib/checkOptions';
 
-export default function (handler, _config = {}) {
+export default function(handler, _config = {}) {
     checkOptions(_config);
     const config = Object.assign({}, defaults, _config);
-
-    return function (component) {
+    
+    return function(component) {
         const queryContainer = withQueryContainer(component);
-
+        
         if (!config.reactive) {
-            const staticQueryContainer = withStaticQuery(queryContainer);
-
-            return function (props) {
+            const staticQueryContainer = withStaticQuery(config)(queryContainer);
+            
+            return function(props) {
                 const query = handler(props);
-
+                
                 return React.createElement(staticQueryContainer, {
                     query,
                     props,
                     config
-                })
-            }
+                });
+            };
         } else {
             return withReactiveQuery(handler, config, queryContainer);
         }
