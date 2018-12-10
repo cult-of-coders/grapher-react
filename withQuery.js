e890755ae561468ccaf2c684408d0c354ed0dd7a
Florian Bienefelt <florian.bienefelt@gmail.com>
2018-12-10 09:55:21 +0100

    Reformat code with 4 spaces

--- withQuery.js
+++ withQuery.js
@@ -9,20 +9,22 @@ import checkOptions from './lib/checkOptions';
 export default function(handler, _config = {}) {
     checkOptions(_config);
     const config = Object.assign({}, defaults, _config);
-    
+
     return function(component) {
         const queryContainer = withQueryContainer(component);
-        
+
         if (!config.reactive) {
-            const staticQueryContainer = withStaticQuery(config)(queryContainer);
-            
+            const staticQueryContainer = withStaticQuery(config)(
+                queryContainer,
+            );
+
             return function(props) {
                 const query = handler(props);
-                
+
                 return React.createElement(staticQueryContainer, {
                     query,
                     props,
-                    config
+                    config,
                 });
             };
         } else {
